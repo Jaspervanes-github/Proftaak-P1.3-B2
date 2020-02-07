45f898022f89382fc0770e8036ef6c036760ceba
Jasper <j.vanes1@student.avans.nl>
2020-02-07 14:55:26 +0100

    changed integerproperty to integer, can now show value in tableview

---
+++ src/Data/Performance.java
@@ -0,0 +1,52 @@
+package Data;
+
+import Data.Person.Artist;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Performance {
+
+    private Integer startTime;
+    private Integer endTime;
+    private Artist artist;
+    private Stage stage;
+
+    public Performance(Integer startTime, Integer endTime, Artist artist, Stage stage) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.artist = artist;
+        this.stage = stage;
+    }
+
+    public Integer getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Integer startTime) {
+        this.startTime = startTime;
+    }
+
+    public Integer getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Integer endTime) {
+        this.endTime = endTime;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
