04a4dc520f521c97d2f407b927450489a17975f2
Robin Koedood <rkoedood8@gmail.com>
2020-03-09 10:07:55 +0100

    [ADD] screenshot van achtergrond

--- Simulation/src/TerrainDemo.java
+++ Simulation/src/TerrainDemo.java
@@ -6,19 +6,26 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class TerrainDemo extends Application {
 
     private TerrainMap map;
     private ResizableCanvas canvas;
+    private BufferedImage imageMap = null;
+    private AffineTransform tx;
+
 
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         this.canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
+
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
         new AnimationTimer() {
             long last = -1;
@@ -41,13 +48,22 @@ public class TerrainDemo extends Application {
     public void init()
     {
             map = new TerrainMap("/Terrain/Map.json");
+        try {
+            imageMap = ImageIO.read(getClass().getResource("/Map.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public void draw(Graphics2D g)
     {
         g.setBackground(Color.black);
         g.clearRect(0,0,(int)canvas.getWidth(), (int)canvas.getHeight());
-        map.draw(g);
+        tx = AffineTransform.getScaleInstance(canvas.getWidth()/imageMap.getWidth(),canvas.getHeight()/imageMap.getHeight());
+        g.drawImage(imageMap, tx,null);
+
     }
 
     public void update(double deltaTime)
